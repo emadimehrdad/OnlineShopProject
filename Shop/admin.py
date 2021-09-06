08c89bf01af60a6d80d634e0ea97ba4b457e4db2
mehrdad <emadi_mehrdad@yahoo.com>
2021-09-07 01:41:30 +0430

    initial commit

---
+++ Shop/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+# Register your models here.
+from Shop import models
+
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+
